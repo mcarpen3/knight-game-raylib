2f4108a377d454aef4feac4404978af74fd4751a
matt.carpenter@digitalally.com <matt.carpenter@digitalally.com>
2025-01-03 15:53:10 -0600

    rename output file

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-gcc -Wall main.c -o raylib-example -I/home/matt/raylib-5.0_linux_amd64/include -L /home/matt/raylib-5.0_linux_amd64/lib/ -lraylib -lm
+gcc -Wall main.c -o main-game -I/home/matt/raylib-5.0_linux_amd64/include -L /home/matt/raylib-5.0_linux_amd64/lib/ -lraylib -lm
 export LD_LIBRARY_PATH=/home/matt/raylib-5.0_linux_amd64/lib
-./raylib-example
+./main-game

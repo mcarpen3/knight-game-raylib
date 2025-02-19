ca4a84b89498bad0e8213d42f12499b18ec25211
matt.carpenter@digitalally.com <matt.carpenter@digitalally.com>
2025-02-19 11:49:47 -0600

    added some more experiments

--- experiments/testCharStarArr.c
+++ experiments/testCharStarArr.c
@@ -10,6 +10,18 @@ int main(void) {
         "'Q'=save sheet",
         "WASD=pan"
     };
-    printf("ARRLEN instructions: %lu", ARRAY_LEN(instructions));
+    char *inst2[][2] = {
+        {"hello", "world"},
+        {"world"},
+    };
+    printf("ARRLEN instructions: %lu\n", ARRAY_LEN(instructions));
+    printf("ARRLEN inst2: %lu\n", ARRAY_LEN(inst2[0]));
+    for (int i = 0; i < ARRAY_LEN(inst2); ++i) {
+        printf("ARRAY_LEN inst2[%d] = %lu\n", i, ARRAY_LEN(inst2[i]));
+        for (int j = 0; j < ARRAY_LEN(inst2[i]); ++j) {
+            if (inst2[i][j] != NULL)
+                printf("%s\n", inst2[i][j]);
+        }
+    }
     return 0;
 }

40fff55e7619b716448d2c0ab91c1e8c98cebb7f
matt.carpenter@digitalally.com <matt.carpenter@digitalally.com>
2025-02-28 13:49:53 -0600

    moved autogetspriterect to utils

--- util/AutoGetSpriteRect.c
+++ util/AutoGetSpriteRect.c
@@ -1,16 +1,19 @@
 #include <raylib.h>
 #include <stdio.h>
-#include "../sprite/sprite.h"
 #include "../datastructs/linkedlist.h"
+#include "../object/mapobject.h"
 
-void GetSpriteBounds(SpriteRect *sprite, List *rects) {
+void DestroyRect(Rectangle *r) {
+    free(r);
+};
+
+void GetSpriteBounds(Image img, List *rects) { 
     int minx;
     int yAlphaSum = 0;
     bool minxFound = false;
-    Image img = LoadImageFromTexture(sprite->texture);
-    for (int w = sprite->r.x; w < sprite->r.width; ++w) {
+    for (int w = 0; w < img.width; ++w) {
         // iterate horizontally
-        for (int h = 0; h < sprite->r.height; ++h) {
+        for (int h = 0; h < img.height; ++h) {
             // iterate vertically
             Color c = GetImageColor(img, w, h);
             yAlphaSum += c.a;
@@ -20,14 +23,15 @@ void GetSpriteBounds(SpriteRect *sprite, List *rects) {
             minxFound = true;
         }
         if (yAlphaSum == 0 && minxFound) {
-            Rectangle *r = (Rectangle *)malloc(sizeof(Rectangle));
-            *r = (Rectangle){
+            Collider *collider = (Collider *)malloc(sizeof(Collider));
+            collider->type = RectType;
+            collider->data.rect = (Rectangle){
                 .x = minx,
                 .y = 0,
                 .width = w - minx - 1,
                 .height = 0
             };
-            list_ins_next(rects, list_tail(rects), r);
+            list_ins_next(rects, list_tail(rects), collider);
             minxFound = false;
         }
         yAlphaSum = 0;
@@ -36,18 +40,18 @@ void GetSpriteBounds(SpriteRect *sprite, List *rects) {
     while(idx != NULL) {
         int xAlphaSum = 0;
         bool minyFound = false;
-        Rectangle *tmp = (Rectangle *)idx->data;
-        for (int h = sprite->r.y; h < sprite->r.height; ++h) {
-            for (int w = tmp->x; w < tmp->x + tmp->width; ++w) {
+        Collider *tmp = (Collider *)idx->data;
+        for (int h = 0; h < img.height; ++h) {
+            for (int w = tmp->data.rect.x; w < tmp->data.rect.x + tmp->data.rect.width; ++w) {
                 Color c = GetImageColor(img, w, h);
                 xAlphaSum += c.a;
             }
             if (xAlphaSum > 0 && !minyFound) {
-                tmp->y = h;
+                tmp->data.rect.y = h;
                 minyFound = true;
             }
             if (minyFound && (xAlphaSum == 0 || h == img.height - 1)) {
-                tmp->height = h - tmp->y;
+                tmp->data.rect.height = h - tmp->data.rect.y;
                 break;
             }
             xAlphaSum = 0;
